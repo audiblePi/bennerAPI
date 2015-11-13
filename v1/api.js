94226d94116f992b7502b79cffe01590901d8e5d
B Hicks <b@audiblePi.com>
2015-11-12 19:12:48 -0500

    Updated functionality with text field and select

--- v1/api.js
+++ v1/api.js
@@ -6,14 +6,20 @@ jQuery(function($){
 	var left;
 	var width = 500;
 	var height = 650;
-    var button = $('._bcg-vsb');
-	var glass_id = button.attr('id');
+	var input = $('#_bcg-product-code');
+	var glass_id = input.val();
 	var url = apiurl + '/api/v1/' + glass_id;
+	var button = $('<div class="_bcg-vsb">Decorate Now</div>');
     var response_code = $('<div class="_bcg-response-code"><br></div>');
-    var buttonStyle = $("<style>._bcg-vsb{background: -webkit-linear-gradient(#1CA8E0 0px, #069EDB 1%, #0AA2DD 9%, #0B7CC1 100%) repeat scroll 0 0; background: -o-linear-gradient(#1CA8E0 0px, #069EDB 1%, #0AA2DD 9%, #0B7CC1 100%) repeat scroll 0 0; background: linear-gradient(#1CA8E0 0px, #069EDB 1%, #0AA2DD 9%, #0B7CC1 100%) repeat scroll 0 0; filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#1CA8E0',endColorstr='#0B7CC1'); border: 1px solid #1ca8e0; border-radius: 5px; color: #fff; cursor: pointer; display: block; font-size: 21px; padding: 6px 40px; text-decoration: none; text-shadow: 2px 2px 2px #06639f; } ._bcg-vsb:hover{background: linear-gradient(to bottom,#1ca8e0 0px,#069edb 1%,#0588cb 9%,#0b7cc1 100%) repeat scroll 0 0; background: -moz-linear-gradient(to bottom,#1ca8e0 0px,#069edb 1%,#0588cb 9%,#0b7cc1 100%) repeat scroll 0 0; background: -webkit-gradient(linear,left top,left bottom,from(#0588cb),to(#0b7cc1)); filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#0B7CC1',endColorstr='#1CA8E0'); border: 1px solid #1ca8e0; border-radius: 5px; text-decoration: none; } </style>");
+    var buttonStyle = $("<style>._bcg-vsb{margin:10px 0px;background: -webkit-linear-gradient(#1CA8E0 0px, #069EDB 1%, #0AA2DD 9%, #0B7CC1 100%) repeat scroll 0 0; background: -o-linear-gradient(#1CA8E0 0px, #069EDB 1%, #0AA2DD 9%, #0B7CC1 100%) repeat scroll 0 0; background: linear-gradient(#1CA8E0 0px, #069EDB 1%, #0AA2DD 9%, #0B7CC1 100%) repeat scroll 0 0; filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#1CA8E0',endColorstr='#0B7CC1'); border: 1px solid #1ca8e0; border-radius: 5px; color: #fff; cursor: pointer; display: block; font-size: 21px; padding: 6px 40px; text-decoration: none; text-shadow: 2px 2px 2px #06639f; } ._bcg-vsb:hover{background: linear-gradient(to bottom,#1ca8e0 0px,#069edb 1%,#0588cb 9%,#0b7cc1 100%) repeat scroll 0 0; background: -moz-linear-gradient(to bottom,#1ca8e0 0px,#069edb 1%,#0588cb 9%,#0b7cc1 100%) repeat scroll 0 0; background: -webkit-gradient(linear,left top,left bottom,from(#0588cb),to(#0b7cc1)); filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#0B7CC1',endColorstr='#1CA8E0'); border: 1px solid #1ca8e0; border-radius: 5px; text-decoration: none; } </style>");
 	var responseStyle = $("<style>._bcg-response-code{display:none; border-radius: 5px; border: 1px solid silver; margin-top: 20px; width: 100%; padding: 6px 40px; font-size: 21px;} </style>");
 	var mask = $('<div class="_bcg-mask"><style>body ._bcg-mask{z-index: 20001;position: absolute; top: 0; left: 0; background-color: black; background-image: radial-gradient(circle farthest-corner, #000000, #4A4A4A); opacity: 0.80;}</style></div>');
-	var label, select, labelSelectWrapper;
+	var label, select, selectWrapper;
+	var textLabel, textField, textWrapper;
+
+	//find these first
+	findSelectOption();
+	findTextField();
 
 	if (window.outerWidth) {
         left = Math.round((window.outerWidth - width) / 2) + window.screenX;
@@ -23,7 +29,15 @@ jQuery(function($){
         top = Math.round((window.screen.height - height) / 2);
     }
 
-	button.after(response_code, buttonStyle, responseStyle);
+	select.change(function(){
+		var val = $(this).val();
+		var text = $(this).children('option:selected').text();
+		if (text.toLowerCase().indexOf("yes") >= 0){
+			select.after(button);
+			button.after(response_code, buttonStyle, responseStyle);
+		}
+	});
+
 	button.click(function(e){
 		$('body').append(mask);
 		mask.css('width', document.body.scrollWidth + 'px');
@@ -41,11 +55,37 @@ jQuery(function($){
 	    setTimeout(function(){ win.postMessage("Hello", apiurl); }, 3000);
 	});
 
+	//Refactor this/ functionaility depends on finding the select option
 	function findSelectOption(){
 	  	label = $('body').find('label:contains("Custom Decoration")');
-		select = label.parent().parent().find('select');
-		labelSelectWrapper = label.parent().parent();
-		
+	  	if (label.length){
+	  		console.log("label found");
+			select = label.parent().parent().find('select');
+			if (select.length){
+				console.log('select option found');
+				selectWrapper = label.parent().parent();
+				if (selectWrapper.length)
+					console.log("select wrapper found");
+			}	
+		}
+		else
+			console.log('unable to find the right label');
+	}
+
+	function findTextField(){
+	  	textLabel = $('body').find('label:contains("Decoration Code")');
+	  	if (textLabel.length){
+	  		console.log("text label found");
+			textField = textLabel.parent().parent().find('input');
+			if (textField.length){
+				console.log('text field found');
+				textWrapper = label.parent().parent();
+				if (textWrapper.length)
+					console.log("text wrapper found");
+			}	
+		}
+		else
+			console.log('unable to find the right text label');
 	}
 
 	function receiveMessage(event){
@@ -58,10 +98,8 @@ jQuery(function($){
 	}
 
 	function showResponse(data){
-		$('._bcg-response-code').html("Your decoration code: " + data);
-		$('._bcg-response-code').show();
+		textField.val(data);
 	}
 
-	findSelectOption();
 	window.addEventListener("message", receiveMessage, false);
 });
